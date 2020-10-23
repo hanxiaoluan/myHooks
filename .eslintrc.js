4cfff399263865ce93670b119ac44dbb867fb125
小白 <1140279968@qq.com>
2020-10-23 14:00:38 +0800

    feat: 🎸 初步完成useChecked的封装

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:react/recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
-		'eslint-config-prettier'
 	],
 	'globals': {
 		'Atomics': 'readonly',
--- src/hooks/useChecked/useChecked.ts
+++ src/hooks/useChecked/useChecked.ts
@@ -1,18 +1,71 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import React, { useState, useEffect, useCallback, useMemo, useReducer } from 'react'
 
+const CHECKED_CHANGE = 'CHECKED_CHANGE'
+const CHECKED_ALL_CHANGE = 'CHECKED_ALL_CHANGE'
+const SET_CHECKED_MAP = 'SET_CHECKED_MAP'
 interface Option {
     key?: string
 }
 type CheckedMap = { [id: number]: boolean }
 type FilterCheckedFunc<T> = (item: T) => boolean
-const useChecked = <T extends Record<string, any>>(dataSource: T[], { key = 'id' }: Option = {}) => {
-    const [checkedMap, setCheckedMap] = useState<CheckedMap>({})
-    const onCheckAllChange = (newCheckedAll: boolean) => {
-        const newCheckedMap: CheckedMap = {}
-        dataSource.map(dataItem => { newCheckedMap[dataItem.id] = newCheckedAll })
-        setCheckedMap(newCheckedMap)
+type CheckedChange<T> = {
+    type: typeof CHECKED_CHANGE
+    payload: {
+        cartItem: T
+        checked: boolean
     }
+}
+type CheckedAllChange = {
+    type: typeof CHECKED_ALL_CHANGE
+    payload: boolean
+}
+type SetCheckedMap = {
+    type: typeof SET_CHECKED_MAP
+    payload: CheckedMap
+}
+type Action<T> = CheckedChange<T> | CheckedAllChange | SetCheckedMap
 
+const useChecked = <T extends Record<string, any>>(dataSource: T[], { key = 'id' }: Option = {}) => {
+
+    const [checkedMap, dispatch] = useReducer((checkedMapParam: CheckedMap, action: Action<T>) => {
+
+        switch (action.type) {
+            case CHECKED_CHANGE: {
+                const { payload } = action
+                const { cartItem, checked } = payload
+                const { [key]: id } = cartItem
+                return {
+                    ...checkedMapParam,
+                    [id]: checked
+                }
+            }
+            case CHECKED_ALL_CHANGE: {
+                const { payload: newCheckedAll } = action
+                const newCheckedMap: CheckedMap = {}
+                if (newCheckedMap) {
+                    dataSource.map((cartItem) => {
+                        newCheckedMap[cartItem.id] = newCheckedAll
+                    })
+                }
+                return newCheckedMap
+            }
+            case SET_CHECKED_MAP: {
+                return action.payload
+            }
+            default:
+                return checkedMapParam
+        }
+    }, {})
+
+    const onCheckAllChange = (newCheckedAll: boolean) => {
+        dispatch({
+            type: CHECKED_ALL_CHANGE,
+            payload: newCheckedAll
+        })
+    }
+    const onCheckedChange = (cartItem: T, checked: boolean) => {
+        dispatch({ type: CHECKED_CHANGE, payload: { cartItem, checked } })
+    }
     const filterChecked = useCallback((func: FilterCheckedFunc<T> = () => true) => {
         return Object.entries(checkedMap)
             .filter(entries => Boolean(entries[1]))
