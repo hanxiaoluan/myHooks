1f1dbd537c368b0e3a0a2642128c5e2c395ed3d1
小白 <1140279968@qq.com>
2020-11-16 21:36:58 +0800

    feat: 🎸 完成useIntersection的自定义hook
    完成useIntersection的自定义hook

---
+++ src/hooks/useIntersection/useIntersection.ts
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+const useIntersection = (
+	ref: React.RefObject<HTMLElement>,
+	options: IntersectionObserverInit
+): IntersectionObserverEntry|null => {
+	const [ intersectionObserverEntry, setIntersectionObserverEntry ] = useState<IntersectionObserverEntry|null>(null)
+	useEffect(() => {
+		if(ref.current && typeof IntersectionObserver === 'function'){
+			const handler = (entries: IntersectionObserverEntry[]) => {
+				setIntersectionObserverEntry(entries[0])
+			}
+            
+			const observer = new IntersectionObserver(handler, options)
+			observer.observe(ref.current)
+			return () => {
+				setIntersectionObserverEntry(null)
+				observer.disconnect()
+			}
+		}
+		return () => {}
+	}, [ ref.current, options.threshold, options.root, options.rootMargin ])
+    
+	return intersectionObserverEntry
+	
+}
+
+export default useIntersection
